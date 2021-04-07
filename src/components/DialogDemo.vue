eef0d6cab3270b1496829c6e8861fba94dab256e
Quincy <kuncc@foxmail.com>
2021-04-07 09:56:33 +0800

    update: Dialog 样式及基础功能

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,24 @@
 <template>
-  <Dialog/>
+  <h1>Dialog 示例</h1>
+  <h3>示例1</h3>
+  <div>
+    <Button @click="toggle">toggle</Button>
+    <Dialog :visible="visible"/>
+  </div>
 </template>
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
+import {ref} from 'vue';
 
 export default {
-  components: {Dialog}
+  components: {Dialog, Button},
+  setup() {
+    const visible = ref(false);
+    const toggle = () => {
+      visible.value = !visible.value;
+    };
+    return {visible, toggle};
+  }
 };
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,7 +1,91 @@
 <template>
-  <div>Dialog</div>
+  <template v-if="visible">
+    <div class="xiyo-dialog-overlay"></div>
+    <div class="xiyo-dialog-wrapper">
+      <header>标题 <span class="xiyo-dialog-close"/></header>
+      <main>
+        <p>第一行字</p>
+        <p>第二行字</p>
+      </main>
+      <footer>
+        <Button level="main">OK</Button>
+        <Button>Cancel</Button>
+      </footer>
+    </div>
+  </template>
 </template>
 
 <script lang="ts">
-export default {};
+import Button from './Button.vue';
+
+export default {
+  components: {Button},
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
 </script>
+
+<style lang="scss">
+.xiyo-dialog-overlay {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  background: fade_out(black, 0.6);
+}
+.xiyo-dialog-wrapper {
+  min-width: 15em;
+  max-width: 90%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 11;
+  background: #fff;
+  border-radius: 6px;
+  box-shadow: 0 0 3px fade_out(black, 0.6);
+  header {
+    padding: 12px 16px;
+    font-size: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  main {
+    padding: 12px 16px;
+  }
+  footer {
+    padding: 12px 16px;
+    text-align: right;
+  }
+  .xiyo-dialog-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+}
+</style>
