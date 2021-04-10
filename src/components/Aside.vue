8d4ae8f35cf67b7e6e5997c5642147776c4ead34
Quincy <kuncc@foxmail.com>
2021-04-10 20:58:15 +0800

    update: 将 Aside 模块化并添加了动画

---
+++ src/components/Aside.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="aside" :class="{asideVisible}">
+    <h2>文档</h2>
+    <ol>
+      <li>
+        <router-link to="/doc/intro">介绍</router-link>
+      </li>
+      <li>
+        <router-link to="/doc/install">安装</router-link>
+      </li>
+      <li>
+        <router-link to="/doc/get-started">开始使用</router-link>
+      </li>
+    </ol>
+    <h2>组件列表</h2>
+    <ol>
+      <li>
+        <router-link to="/doc/switch">Switch 开关</router-link>
+      </li>
+      <li>
+        <router-link to="/doc/button">Button 按钮</router-link>
+      </li>
+      <li>
+        <router-link to="/doc/dialog">Dialog 对话</router-link>
+      </li>
+      <li>
+        <router-link to="/doc/tabs">Tabs 标签页</router-link>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script lang="ts">
+import {inject, Ref} from 'vue';
+
+export default {
+  setup() {
+    const asideVisible = inject<Ref<boolean>>('asideVisible');
+    return {asideVisible};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.aside {
+  background: #fff;
+  width: 150px;
+  height: 100%;
+  padding: 56px 16px 16px 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9;
+  box-shadow: 1px 0 1px rgba(0, 0, 0, 0.08);
+  transition: all .3s;
+  h2 {
+    margin: 16px 0 8px;
+  }
+  li {
+    padding: 4px 0;
+    .router-link-active {
+      color: #FF8D78;
+    }
+  }
+}
+@media (max-width: 500px) {
+  .aside {
+    transform: translateX(-100%);
+    &.asideVisible {
+      transform: translateX(0);
+    }
+  }
+}
+</style>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,35 +1,7 @@
 <template>
   <TopNav toggleAsideButtonVisible underlineVisible/>
   <div class="content">
-    <aside v-if="asideVisible">
-      <h2>文档</h2>
-      <ol>
-        <li>
-          <router-link to="/doc/intro">介绍</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/install">安装</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/get-started">开始使用</router-link>
-        </li>
-      </ol>
-      <h2>组件列表</h2>
-      <ol>
-        <li>
-          <router-link to="/doc/switch">Switch 开关</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/button">Button 按钮</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/dialog">Dialog 对话</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/tabs">Tabs 标签页</router-link>
-        </li>
-      </ol>
-    </aside>
+    <Aside/>
     <main>
       <router-view/>
     </main>
@@ -38,36 +10,16 @@
 
 <script lang="ts">
 import TopNav from '../components/TopNav.vue';
-import {inject, Ref} from 'vue';
+import Aside from '../components/Aside.vue';
 
 export default {
-  components: {TopNav},
-  setup() {
-    const asideVisible = inject<Ref<boolean>>('asideVisible');
-    return {asideVisible};
-  }
+  components: {TopNav, Aside},
 };
 </script>
 
 <style lang="scss" scoped>
 .content {
   margin: 66px 16px 16px 166px;
-  aside {
-    background: #fff;
-    width: 150px;
-    height: 100%;
-    padding: 56px 16px 16px 16px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 9;
-    h2 {
-      margin: 16px 0 8px;
-    }
-    li {
-      padding: 4px 0;
-    }
-  }
   main {
     padding: 20px 14px;
     transition: all .3s;
