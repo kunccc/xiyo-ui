90711059eec07af1ab6ee83e380b7f9c821fef65
Quincy <kuncc@foxmail.com>
2021-04-10 11:19:30 +0800

    update: 展示源代码并且高亮

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,16 +1,60 @@
 <template>
-  <Switch v-model:value="isOpen"/>
+  <h1>Switch 开关</h1>
+  <p>用于两种对立状态间的切换</p>
+  <div class="demo">
+    <h2>基本用法</h2>
+    <div class="demo-content">
+      <component :is="Switch1Demo"/>
+      <div class="demo-content-action">
+        <Button theme="link">查看代码</Button>
+      </div>
+      <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')"/>
+    </div>
+  </div>
+  <div class="demo">
+    <h2>支持 disabled</h2>
+    <div class="demo-content">
+      <component :is="Switch2Demo"/>
+      <div class="demo-content-action">
+        <Button theme="link">查看代码</Button>
+      </div>
+      <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')"/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Switch from '../lib/Switch.vue';
-import {ref} from 'vue';
+import Button from '../lib/Button.vue';
+import Switch1Demo from './Switch1.demo.vue';
+import Switch2Demo from './Switch2.demo.vue';
+import 'prismjs';
+import 'prismjs/themes/prism.css';
+
+const Prism = (window as any).Prism;
 
 export default {
-  components: {Switch},
+  components: {Button},
   setup() {
-    const isOpen = ref(false);
-    return {isOpen};
+    return {Switch1Demo, Switch2Demo, Prism};
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.demo {
+  margin: 60px 0;
+  &-content {
+    border: 1px solid #ddd;
+    padding: 16px;
+    margin-top: 16px;
+    &-action {
+      display: flex;
+      justify-content: center;
+    }
+    pre {
+      font-size: 14px;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+    }
+  }
+}
+</style>
